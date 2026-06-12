4ecadc96aef27f709431a5ce08914142e219dd44
kofra002 <kofra002@osloskolen.no>
2026-06-12 15:25:29 +0200

    general logic and I2C for BNO085

--- backup/backup.ino
+++ backup/backup.ino
@@ -1,31 +1,48 @@
 /*
   Time interval: https://stackoverflow.com/a/10773464
   SD: https://docs.arduino.cc/learn/programming/sd-guide/ & https://github.com/PaulStoffregen/Teensyduino_Examples/blob/master/USB_Media_Transfer/Basic_SD_Card/Basic_SD_Card.ino
-  GY-521: https://mschoeffler.com/2017/10/05/tutorial-how-to-use-the-gy-521-module-mpu-6050-breakout-board-with-the-arduino-uno/ & https://projecthub.arduino.cc/Nicholas_N/how-to-use-the-accelerometer-gyroscope-gy-521-647e65
+  BNO085: https://adafruit.github.io/Adafruit_BNO08x/html/class_adafruit___b_n_o08x.html & https://github.com/adafruit/Adafruit_BNO08x/tree/master/examples
 */
 
 #include <iostream>
 #include <string>
 #include <SD.h>
-#include <Wire.h>
+#include <Adafruit_BNO08x.h>
+#include <Adafruit_BMP280.h>
+
+#define BNO085_INT 2
+#define BNO085_RESET -1
+
+Adafruit_BNO08x bno085(BNO085_RESET);
+sh2_SensorValue_t sensorValue;
 
 unsigned long previousMillis = 0;
 long interval = 100;
 
-const int MPU_ADDR = 0x68; // I2C address of accelerometer
-
-int16_t accelerometer_x, accelerometer_y, accelerometer_z;
-int16_t gyro_x, gyro_y, gyro_z;
-int16_t temperature;
-
 void setup() {
   Serial.begin(9600);
-  SD.begin(BUILTIN_SDCARD);
-  Wire.begin();
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(BUILTIN_SDCARD)) {
+    Serial.println("Failed to connect SD card")
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("SD card found!")
+
+  Serial.println("Initializing BNO085...");
+  if (!bno085.begin_I2C()) {
+    Serial.println("Failed to connect BNO085");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("BNO085 found!");
 }
 
 void loop() {
@@ -34,11 +51,23 @@ void loop() {
   if (currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;
 
-    accelerometer();
+    IMU();
 
     //if (accelerationThreshold()) {
     //  parachute();
     //}
+  }
+}
+
+void setReports() {
+  if (!bno085.enableReport(SH2_ACCELEROMETER)) {
+    Serial.println("Could not enable accelerometer");
+  }
+  if (!bno085.enableReport(SH2_GYROSCOPE_CALIBRATED)) {
+    Serial.println("Could not enable gyroscope");
+  }
+  if (!bno085.enableReport(SH2_ROTATION_VECTOR)) {
+    Serial.println("Could not enable rotation vector");
   }
 }
 
@@ -47,42 +76,33 @@ void loop() {
 //  accelerometer();
 //}
 
-//float accelerometer() {
-void accelerometer() {
-  // get acceleration and save to SD
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 14, true);
+void IMU() {
+  if (bno085.wasReset()) {
+    setReports();
+  }
 
-  accelerometer_x = Wire.read()<<8 | Wire.read();
-  accelerometer_y = Wire.read()<<8 | Wire.read();
-  accelerometer_z = Wire.read()<<8 | Wire.read();
-  gyro_x = Wire.read()<<8 | Wire.read();
-  gyro_y = Wire.read()<<8 | Wire.read();
-  gyro_z = Wire.read()<<8 | Wire.read();
-  temperature = Wire.read()<<8 | Wire.read();
+  if (!bno085.getSensorEvent(&sensorValue)) {
+    return;
+  }
 
-  Serial.print("Accelerometer: ");
-  Serial.print("X = "); Serial.print(accelerometer_x);
-  Serial.print(" | Y = "); Serial.print(accelerometer_y);
-  Serial.print(" | Z = "); Serial.println(accelerometer_z);
-
-  //Serial.print("Gyroscope: ");
-  //Serial.print("X = "); Serial.print(gyro_x);
-  //Serial.print(" | Y = "); Serial.print(gyro_y);
-  //Serial.print(" | Z = "); Serial.println(gyro_z);
-
-  //Serial.print("Temperature: ");
-  //Serial.print("Temperature = "); Serial.print(temperature);
-
-  Serial.println(" ");
+  accelerometer();
+  gyroscope();
+  angular();
 }
 
-//void transmit(const std::string& message) {
-//  // send notifications to control system
-//}
+void accelerometer() {
+  accelerometerFile = SD.open("accelerometer.txt", FILE_WRITE);
+  if (accelerometerFile) {
+    accelerometerFile.println()
+  }
+}
 
+void gyroscope() {
+
+}
+void angular() {
+
+}
 //void parachute() {
 //  // Detach parachute
 //}
